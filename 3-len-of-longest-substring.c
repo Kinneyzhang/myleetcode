4d6a0b5d48a227e7d1437d1b672e0ca54b011136
Kinneyzhang <kinneyzhang666@gmail.com>
2022-07-23 11:48:14 +0800

    update

--- 3-len-of-longest-substring.c
+++ 3-len-of-longest-substring.c
@@ -1,8 +1,6 @@
-/**
- * 无重复字符的最长子串
- * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
- * 滑动窗口问题
- */
+/* 无重复字符的最长子串 */
+/* 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 */
+/* 滑动窗口问题 */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -59,5 +57,6 @@ int lengthOfLongestSubstring (char* s) {
 
 int main () {
   char* s = "fababcbeccabeab";
+  printf("ddd");
   printf("length: %d\n", lengthOfLongestSubstring(s));
 }
---
+++ 3-len-of-longest-substring.py
@@ -0,0 +1,14 @@
+'''
+无重复字符的最长子串
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+滑动窗口问题
+'''
+
+def length_of_longest_substring ():
+   pass
+
+def main ():
+   pass
+
+if __name__ == '__main__':
+   main()
